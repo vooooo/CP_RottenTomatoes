31da39b9a637b3ce8fc7d24be355a2de55114ee9
Vu Nguyen <vubnguyen@gmail.com>
2015-09-18 23:55:23 -0700

    added pull to refresh

--- RottenTomatoes/MoviesViewController.swift
+++ RottenTomatoes/MoviesViewController.swift
@@ -11,34 +11,29 @@ import UIKit
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
-        
+    var refreshControl: UIRefreshControl!
     var movies: [NSDictionary]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchMovies()
-        
-//        let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")!
-//        let request = NSURLRequest(URL: url)
-//        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
-//            if let json = data {
-//                let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(json, options: []) as! NSDictionary
-//                self.movies = responseDictionary["movies"] as? [NSDictionary]
-//                self.tableView.reloadData()
-//                
-//                NSLog("response: \(self.movies)")
-//            } else {
-//                if let e = error {
-//                    NSLog("Error: \(e)")
-//                }
-//            }
-//        }
         tableView.dataSource = self
         tableView.delegate = self
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "fetchMovies", forControlEvents: UIControlEvents.ValueChanged)
+        
+        let dummyTableVC = UITableViewController()
+        dummyTableVC.tableView = tableView
+        dummyTableVC.refreshControl = refreshControl
+
+        fetchMovies()
+
+        
     }
 
     func fetchMovies() {
         MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        self.movies = []
     
         let url = NSURL(string: "https://gist.githubusercontent.com/timothy1ee/d1778ca5b944ed974db0/raw/489d812c7ceeec0ac15ab77bf7c47849f2d1eb2b/gistfile1.json")!
         let request = NSURLRequest(URL: url)
@@ -48,6 +43,8 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 self.movies = responseDictionary["movies"] as? [NSDictionary]
                 self.tableView.reloadData()
                 sleep(2)
+
+                self.refreshControl.endRefreshing()
                 
                 NSLog("response: \(self.movies)")
                 MBProgressHUD.hideHUDForView(self.view, animated: true)
